679ed8c018369be444051dc102736e0935ad4846
imsanjoykb <sanjoy.eee32@gmail.com>
2021-07-22 23:51:29 +0600

    Flutter ui updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-// Uncomment lines 7 and 10 to view the visual layout at runtime.
-// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
   // debugPaintSizeEnabled = true;
