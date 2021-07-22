7cc4428980645ca5e023c43c9f6c0c36c1b480f0
imsanjoykb <sanjoy.eee32@gmail.com>
2021-07-22 23:53:14 +0600

    Text updated

--- lib/main.dart
+++ lib/main.dart
@@ -66,8 +66,7 @@ class MyApp extends StatelessWidget {
         'Alps. Situated 1,578 meters above sea level, it is one of the '
         'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
         'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
+        'lake, which warms to 20 degrees Celsius in the summer.',
         softWrap: true,
       ),
     );
